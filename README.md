1ae7e9dcd79ea0e69aa0f7e4990682694b51b3b5
Wellington <weelrs8@gmail.com>
2015-04-23 13:30:40 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# libvirt-php
+Libvirt PHP
+===========
+
+Conectando KVM TCP
+------------------
+
+    <?php
+    use LibvirtPHP\App\Libvirt;
+    use LibvirtPHP\Auth\QemuTcp;
+
+    $app = new Libvirt();
+    $app->connect(
+        new QemuTcp(
+            'example.com',
+            [
+                'username' => 'user',
+                'password' => 'pass'
+            ]
+        )
+    );
+
+Obtendo todos os dominios
+-------------------------
+
+    <?php
+
+    $app->getDomains();
+
+Ligando um dominio expecifico
+-----------------------------
+
+    <?php
+
+    $app->getDomainManager('domain_id')->powerOn();
--- src/LibvirtPHP/App/Libvirt.php
+++ src/LibvirtPHP/App/Libvirt.php
@@ -83,4 +83,45 @@ class Libvirt
     {
         return libvirt_version();
     }
+
+    public function newDomain($id, $vcpu, $memory, array $disk, $network, array $path, array $os)
+    {
+        $id = sprintf("vm-%u", $id);
+        $vcpu = (int) $vcpu;
+        $memory = (int) (1024 * (int) $memory);
+        $disk['size'] = (int) $disk['size'];
+
+        $cmd =
+            sprintf(
+                "
+                    nohup virt-install
+                    -n %s -r %u --vcpus=%u --disk path=%s,size=%d
+                    --network %s --location=%s --extra-args \"ks=%s\"
+                    --graphics vnc,listen=0.0.0.0 --noautoconsole
+                    --os-type=%s --os-variant=%s
+                    > %s &
+                ",
+                $id,
+                $memory,
+                $vcpu,
+                $disk['path'],
+                $disk['size'],
+                $network,
+                $path['path'],
+                $path['ks'],
+                $os['type'],
+                $os['variant'],
+                "/logs/{$id}.log"
+            )
+        ;
+        /*
+         * virt-install -n vm-02 -r 1024 --vcpus=1 \
+         * --disk path=/datastore/instances/vm-02.qcow2,size=20 \
+         * --network bridge=virbr0 \
+         * --location=http://172.17.10.110:8001/centos/6/os/x86_64/ \
+         * --extra-args "ks=http://172.17.10.110:8001/centos/6/centos.ks" \
+         * --graphics vnc,listen=0.0.0.0 --noautoconsole \
+         * --os-type=linux --os-variant=rhel6
+         */
+    }
 }
